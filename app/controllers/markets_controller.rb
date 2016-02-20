36393a4b0cc9e971ccae94691e1764a872a2c3ad
alexjones <AkJones2007@gmail.com>
2016-02-20 17:30:35 -0500

    Creates controller for Markets model

---
+++ app/controllers/markets_controller.rb
@@ -0,0 +1,53 @@
+class MarketsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  #Show all markets
+  def index
+    markets = Market.all
+    render json: markets
+  end
+
+  #Show market by id
+  def show
+    market = Market.find(params[:id])
+    render json: market
+  end
+
+  #Add a market
+  def create
+    market = Market.new(market_params)
+
+    if market.save
+      render json: market
+    else
+      render json: market.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    market = Market.find(params[:id])
+
+    if market.update(market_params)
+      render json: market
+    else
+      render json: market.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    market = Market.find(params[:id])
+
+    if market.destroy
+      render text: 'OK', status: :ok
+    else
+      render json: market.errors, status: :not_found
+    end
+  end
+
+  private
+
+  def market_params
+    params.require(:market).permit(:country, :region, :city)
+  end
+
+end
