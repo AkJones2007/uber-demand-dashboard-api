5a5ba89cfeab0b8af4843f840a45eadca0132abf
alexjones <AkJones2007@gmail.com>
2016-02-28 17:46:24 -0500

    Modifies 'last_twenty_four' method to return the found records formatted in a more useful way

--- app/models/demand_log.rb
+++ app/models/demand_log.rb
@@ -3,6 +3,12 @@ class DemandLog < ActiveRecord::Base
 
   def self.last_twenty_four
     logs = DemandLog.where( created_at: 24.hours.ago..Time.now )
+
+    logs.each do |log|
+      time = log.created_at.strftime("%H:%M %p")
+      surge = log.surge_multiplier.round(1).to_f
+      { time: time, surge: surge }
+    end
   end
 
 end
